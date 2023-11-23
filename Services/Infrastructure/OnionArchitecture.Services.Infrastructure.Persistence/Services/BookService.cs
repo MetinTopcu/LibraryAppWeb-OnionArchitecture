83da313ba84489e6bbb46855cd2f33b28488adcd
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 16:48:52 +0300

    Created Services

---
+++ Services/Infrastructure/OnionArchitecture.Services.Infrastructure.Persistence/Services/BookService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using OnionArchitecture.Services.Core.Application.Repositories;
+using OnionArchitecture.Services.Core.Application.Services;
+using OnionArchitecture.Services.Core.Application.UnitOfWorks;
+using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Infrastructure.Persistence.Services
+{
+    public class BookService : Service<Book> , IBookService
+    {
+        private readonly IBookRepository _repository;
+        private readonly IMapper _mapper;
+
+        public BookService(IGenericRepository<Book> repository, IUnitOfWork unitOfWork, IMapper mapper, IBookRepository bookRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _repository = bookRepository;
+        }
+    }
+}
---
+++ Services/Infrastructure/OnionArchitecture.Services.Infrastructure.Persistence/Services/CategoryService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using OnionArchitecture.Services.Core.Application.Repositories;
+using OnionArchitecture.Services.Core.Application.Services;
+using OnionArchitecture.Services.Core.Application.UnitOfWorks;
+using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Infrastructure.Persistence.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IGenericRepository<Category> repository, IUnitOfWork unitOfWork, IMapper mapper, ICategoryRepository categoryRepository) : base(repository, unitOfWork)
+        {
+            _mapper = mapper;
+            _repository = categoryRepository;
+        }
+    }
+}
