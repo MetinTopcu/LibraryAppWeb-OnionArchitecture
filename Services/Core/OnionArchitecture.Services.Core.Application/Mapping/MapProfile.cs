0bbeeb90f3dde140e508e892ac83be51759a27cf
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 16:34:13 +0300

    Mapping

---
+++ Services/Core/OnionArchitecture.Services.Core.Application/Mapping/MapProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using OnionArchitecture.Services.Core.Application.Dtos;
+using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Core.Application.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Book, BookDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
