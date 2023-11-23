4452251ddf9cd8ef8ee6b22d4d94bb5ffee1acf9
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 18:17:44 +0300

    Database Connection

--- Services/Presentation/OnionArchitecture.Services.Presentation.API/Startup.cs
+++ Services/Presentation/OnionArchitecture.Services.Presentation.API/Startup.cs
@@ -2,13 +2,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using OnionArchitecture.Services.Core.Application.Repositories;
+using OnionArchitecture.Services.Core.Application.Services;
+using OnionArchitecture.Services.Core.Application.UnitOfWorks;
+using OnionArchitecture.Services.Infrastructure.Persistence;
+using OnionArchitecture.Services.Infrastructure.Persistence.Repositories;
+using OnionArchitecture.Services.Infrastructure.Persistence.Services;
+using OnionArchitecture.Services.Infrastructure.Persistence.UnitOfWorks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace OnionArchitecture.Services.Presentation.API
@@ -25,6 +34,22 @@ namespace OnionArchitecture.Services.Presentation.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped(typeof(IService<>), typeof(Service<>));
+            services.AddScoped<IBookRepository,BookRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IBookService, BookService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"), configure =>
+            {
+                //configure.MigrationsAssembly("OnionArchitecture.Services.Infrastructure.Persistence");
+                configure.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); // tip güvenli hali
+            }));
+
             services.AddControllers();
         }
 
