cfba61c46aa5a6dd48992635d80acde3b979896c
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 15:41:04 +0300

    Repositories

---
+++ Services/Core/OnionArchitecture.Services.Core.Application/Repositories/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Core.Application.Repositories
+{
+    public interface IBookRepository : IGenericRepository<Book>
+    {
+    }
+}
---
+++ Services/Core/OnionArchitecture.Services.Core.Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Services.Core.Application.Repositories
+{
+    public interface ICategoryRepository : IGenericRepository<Category>
+    {
+
+    }
+}
