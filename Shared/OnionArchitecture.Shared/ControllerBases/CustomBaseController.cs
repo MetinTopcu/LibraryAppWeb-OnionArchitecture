51ec09fdeb877c3a6247a97c8863b21468bb80b5
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 19:13:06 +0300

    Created Shared

---
+++ Shared/OnionArchitecture.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using OnionArchitecture.Shared.Dtos;
+
+namespace OnionArchitecture.Shared.ControllerBases
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(ResponseDto<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
