d30fd0a0660e9c1e3fdfd4136635b1dc8509ad59
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 16:24:10 +0300

    Created Repositories

---
+++ Services/Infrastructure/OnionArchitecture.Services.Infrastructure.Persistence/Repositories/BookRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnionArchitecture.Services.Core.Application.Repositories;
+using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Infrastructure.Persistence.Repositories
+{
+    public class BookRepository : GenericRepository<Book>, IBookRepository
+    {
+        public BookRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ Services/Infrastructure/OnionArchitecture.Services.Infrastructure.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using OnionArchitecture.Services.Core.Application.Repositories;
+using OnionArchitecture.Services.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+
+        }
+    }
+}
