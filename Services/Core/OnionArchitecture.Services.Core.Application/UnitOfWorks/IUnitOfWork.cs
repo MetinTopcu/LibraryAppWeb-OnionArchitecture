18b3858487cde74e08b82817161344a3fea15619
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-23 15:33:16 +0300

    Created Interfaces

---
+++ Services/Core/OnionArchitecture.Services.Core.Application/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Services.Core.Application.UnitOfWorks
+{
+    public interface IUnitOfWork// repository de db verilerinde değişiklik yapıldığında tek tek yapmayıp 
+                                // bütün verileri toplayıp en son savechange savechangeasync yapmayı sağlayan yerdir.
+                                // eğer bir kısımda hata olursa hiçbir değişiklik uygulanmaz.
+    {
+        Task CommitAsync();
+
+        void Commit();
+    }
+}
