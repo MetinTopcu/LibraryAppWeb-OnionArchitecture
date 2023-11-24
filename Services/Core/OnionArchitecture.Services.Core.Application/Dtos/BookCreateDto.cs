b92bfff1c89ab37d46331318b78804f587ab3847
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-24 21:07:00 +0300

    Created BookCreateDto and BookUpdateDto

---
+++ Services/Core/OnionArchitecture.Services.Core.Application/Dtos/BookCreateDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Core.Application.Dtos
+{
+    public class BookCreateDto
+    {
+        public string Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+    }
+}
