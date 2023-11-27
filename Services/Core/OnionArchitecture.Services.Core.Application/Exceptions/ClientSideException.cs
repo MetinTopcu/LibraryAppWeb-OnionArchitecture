3d022a342f6c2e51cbd98c4e2c05610fe5fccbda
MetinTopcu <70098939+MetinTopcu@users.noreply.github.com>
2023-11-27 20:32:00 +0300

    Created ClientSideException

---
+++ Services/Core/OnionArchitecture.Services.Core.Application/Exceptions/ClientSideException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnionArchitecture.Services.Core.Application.Exceptions
+{
+    public class ClientSideException : Exception
+    {
+        public ClientSideException(string message):base(message)
+        { 
+
+        }
+    }
+}
